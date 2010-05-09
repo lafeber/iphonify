aacb5b6dad5056adf1f5395c803247f2d82f0559
Martijn Lafeber <lafeber@gmail.com>
2010-05-09 13:04:13 +0200

    applied new design

---
+++ public/inc/mail.config.php
@@ -0,0 +1,10 @@
+<?php
+
+	$youremail		= "admin@yourwebsite.com"; // e-mail address to send subscription request
+	$emailtitle		= "Please Update Me"; // subject of e-mail you will recieve
+	$emailmsg		= "I'm aware that your website is under construction. However, can you please update me when the website is live? Thank You!";
+	$autoresponse	= "yes"; // yes or no
+	$responsetitle	= "Thank You for Subscribing!"; // subject of auto response e-mail
+	$responsemsg	= "Thank you for subscribing! You will be notified as soon as our website is live.";
+	
+?>
